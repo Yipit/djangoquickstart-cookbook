d9557babcda82c6aae2191cd19924c699c081e08
Zach Smith <zmsmith27@gmail.com>
2011-11-08 11:54:48 -0500

    Commentary

--- recipes/default.rb
+++ recipes/default.rb
@@ -8,9 +8,13 @@
 #
 
 
+# Calculate some locations based on set attributes
+
 node.env_name = "#{node.app_name}-env"
 node.project_home = "#{node.env_root}/#{node.env_name}"
 node.app_home = "#{node.project_home}/#{node.app_name}"
+
+# Install some packages
 
 ['binutils', 'chkconfig', 'libcurl4-gnutls-dev', 'libxslt1-dev',
 'ntp','python-dev','python-virtualenv', 'python-setuptools', 'rubygems', 'vim', 'git'].each do |pkg|
@@ -18,6 +22,9 @@ node.app_home = "#{node.project_home}/#{node.app_name}"
           :install
       end
 end
+
+# Supervisor installation has a bug, so we need to 
+# install elementtree before we can install supervisor
 
 python_pip "elementtree" do
   action :install
@@ -27,10 +34,17 @@ package "supervisor" do
     action :install
 end
 
+# Set up a command to update supervisor on config 
+# changes, but don't actually do anything now
+
 execute "supervisor update" do
   command "sudo supervisorctl reread && sudo supervisorctl update"
   action :nothing
 end
+
+# This gives us a nicely colored prompt that we can use
+# to indicate different environments. It also gives 
+# us some more info about the machin on the command line
 
 template "/home/ubuntu/.bashrc" do
   source "bashrc.erb"
@@ -43,6 +57,8 @@ template "/home/ubuntu/.bashrc" do
     :prompt_color => node.prompt_color)
 end
 
+# To see exactly what this does, checkout provieders/build_repo.rb
+# This only runs if the project home directory doesn't exist yet
 djangoquickstart_build_repo node.branch do
   action :add
   settings node.settings
@@ -55,6 +71,8 @@ end
 package "nginx" do
     :install
 end
+
+# Set up nginx sites-enables and retsart on changes
 
 template "/etc/nginx/sites-enabled/default" do
   source "nginx-default.erb"
@@ -70,6 +88,8 @@ service 'nginx' do
   supports :restart => true, :reload => true
   action :enable
 end
+
+# Set up gunicorn through supervisor and restart on changes
 
 template "/etc/supervisor/conf.d/gunicorn.conf" do
   source "gunicorn.conf.erb"
